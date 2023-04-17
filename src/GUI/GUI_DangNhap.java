060a90a75d42d96a80e2c09307d98d3f44291247
Minh Phương Đặng <minhphuong4203@gmail.com>
2023-04-17 21:46:26 +0700

    add GUI dangNhap

---
+++ src/GUI/GUI_DangNhap.java
@@ -0,0 +1,86 @@
+package GUI;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class GUI_DangNhap extends JFrame {
+
+	private JTextField txtUsername;
+	private JPasswordField txtPassword;
+	private JButton btnLogin;
+	private JButton btnCancel;
+	private ImageIcon iconXoa = new ImageIcon(
+			new ImageIcon("picture/delete-icon.png").getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH));
+	private ImageIcon iconDangNhap = new ImageIcon(
+			new ImageIcon("picture/login-icon.jpg").getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH));
+
+	public GUI_DangNhap() {
+		setSize(420, 300);
+		setTitle("Dang Nhap");
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		JPanel contentPanel = new JPanel();
+		contentPanel.setLayout(null);
+
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPanel.setBounds(0, 0, 450, 250);
+
+		JLabel lblNewLabel = new JLabel("Đăng Nhập");
+		lblNewLabel.setForeground(Color.BLUE);
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 27));
+		lblNewLabel.setBounds(131, 11, 155, 51);
+		contentPanel.add(lblNewLabel);
+
+		JLabel lblNewLabel_1 = new JLabel("Tên đăng nhập:");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_1.setBounds(50, 83, 102, 28);
+		contentPanel.add(lblNewLabel_1);
+
+		txtUsername = new JTextField();
+		txtUsername.setColumns(10);
+		txtUsername.setBounds(160, 85, 185, 29);
+		contentPanel.add(txtUsername);
+
+		JLabel lblNewLabel_2 = new JLabel("Mật khẩu:");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_2.setBounds(66, 122, 102, 34);
+		contentPanel.add(lblNewLabel_2);
+
+		txtPassword = new JPasswordField();
+		txtPassword.setColumns(10);
+		txtPassword.setBounds(160, 127, 185, 28);
+		contentPanel.add(txtPassword);
+
+		btnLogin = new JButton("Đăng nhập", iconDangNhap);
+		btnLogin.setActionCommand("OK");
+		btnLogin.setBounds(224, 200, 120, 39);
+		contentPanel.add(btnLogin);
+
+		btnCancel = new JButton("Huỷ", iconXoa);
+		btnCancel.setActionCommand("Cancel");
+		btnCancel.setBounds(70, 200, 100, 39);
+		contentPanel.add(btnCancel);
+
+		this.add(contentPanel);
+
+		contentPanel.setBackground(new Color(255, 204, 163));
+
+		this.setBackground(new Color(255, 230, 179));
+
+	}
+
+	public static void main(String[] args) {
+		new GUI_DangNhap().setVisible(true);
+	}
+}
